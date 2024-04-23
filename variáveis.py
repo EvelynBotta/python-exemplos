58179c8f468505757ef59b08e077ab5083b80e13
EvelynBotta <evelyn.ferr.b@gmail.com>
2024-04-23 16:40:36 -0300

    criação variáveis.py

---
+++ variáveis.py
@@ -0,0 +1,12 @@
+nome="Evelyn Botta"
+idade=16
+email="ferreirabottaevelyn@050508"
+telefone="17 0000-00"
+nota=8.99
+aprovada=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovada)
