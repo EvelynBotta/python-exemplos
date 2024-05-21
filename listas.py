e84e8bedbce30615900fc54b2419f6b305ee5018
EvelynBotta <evelyn.ferr.b@gmail.com>
2024-05-21 14:53:18 -0300

    listas.py

---
+++ listas.py
@@ -0,0 +1,8 @@
+aluno = ["Evelyn Botta", 4, 8.5, False]
+print(f'Nome: {aluno[0]}')
+print(f'Faltas: {aluno[1]}')
+print(f'Média: {aluno[2]}')
+if aluno[3] == True:
+    print(f'Aprovado: Sim')
+else:
+    print(f'Sprovado: Não')
